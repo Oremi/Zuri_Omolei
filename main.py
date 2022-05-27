4d998a2e38a29568351b98987d6452df5a737307
Oremi <40256016+Oremi@users.noreply.github.com>
2022-05-27 20:17:10 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+#code by Godwin Remilekun Omolei
+#Student ID: 14G021587MXP
+
+def find_anagram(word, anagram):
+    # sort the word and anagram for comparison
+    if(sorted(word) == sorted(anagram)):
+        return True
+    else:
+        return False
+
+print("This is a program to find if a word matches an anagram\n")
+
+word = input("please enter a word to check if it matches an anagram: ")
+print("This is the word:", word)
+    
+anagram = input("please enter an anagram to check with the word: ")
+print("This is the anagram:", anagram)
+
+print(f"\nchecking... {word} and {anagram}")
+
+print(f"checked\n")
+print(f"Result: {find_anagram(word, anagram)}")
