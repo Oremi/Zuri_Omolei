8819964d9d09d519e938136e0edff1383cdb0f18
Oremi <40256016+Oremi@users.noreply.github.com>
2022-05-27 22:14:07 +0100

    Update main.py
    Corrected Student ID in the comments

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # find_anagrams("below", "elbow") --> True
 
 #code by Godwin Remilekun Omolei
-#Student ID: 14G021587MXP
+#Student ID: I4G021587MXP
 
 def find_anagram(word, anagram):
     # sort the word and anagram for comparison
